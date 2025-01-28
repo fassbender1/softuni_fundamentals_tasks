4fafe52eadf81a33b69c0b2e39752cd3f5eac123
fassbender1 <civello6969@gmail.com>
2025-01-28 04:19:31 +0200

    1
    1

--- HelloWorld/Fundamentals/data_types_variables/next_happy_year.py
+++ HelloWorld/Fundamentals/data_types_variables/next_happy_year.py
@@ -1,1 +1,10 @@
-asdsdafsdaffdsafdsa
+happy_year = int(input())
+
+while True:
+    happy_year += 1
+    year_str = str(happy_year)
+
+    if len(set(year_str)) == len(year_str):
+        break
+
+print(happy_year)
