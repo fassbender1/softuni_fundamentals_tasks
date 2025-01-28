23a120ffa76ac30f93a9914b3d88e5fd0378b616
fassbender1 <87240060+fassbender1@users.noreply.github.com>
2025-01-28 04:02:15 +0200

    dsf
    fd

--- HelloWorld/Fundamentals/data_types_variables/special_numbers.py
+++ HelloWorld/Fundamentals/data_types_variables/special_numbers.py
@@ -2,12 +2,12 @@ num = int(input())
 
 for i in range(1, num + 1):
     sum_of_digits = 0
-    digits = num
+    digits = i
     while digits > 0:
         sum_of_digits += digits % 10
         digits = int(digits / 10)
 
-    if i % 5 == 0 or i % 7 == 0 or i % 11 == 0:
+    if (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11):
         print(f"{i} -> True")
     else:
         print(f"{i} -> False")
